44384d90820d8c399f74a62ba97a0ad8b4658703
Kirill Fuchs <kfuchs@fuzzproductions.com>
2016-11-16 14:22:05 -0500

    fixed some more delets

---
+++ deletes/bash-setup-delete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -f
+
+rm -f $HOME/.git-completion.bash
+rm -f $HOME/.bash_aliases
+rm -f $HOME/.bash_profile
--- deletes/php-apache-delete.sh
+++ deletes/php-apache-delete.sh
@@ -6,31 +6,9 @@ rm -r /usr/local/etc/apache2/2.4
 
 # Uninstall php70
 brew uninstall php70
-brew uninstall php70-couchbase
-brew uninstall php70-event
-brew uninstall php70-gearman
-brew uninstall php70-geoip
-brew uninstall php70-imagick
-brew uninstall php70-mcrypt
-brew uninstall php70-mongodb
-brew uninstall php70-pdo-pgsql
-brew uninstall php70-redis
-brew uninstall php70-solr
-brew uninstall php70-xdebug
 
 # Uninstall php56
 brew uninstall php56
-brew uninstall php56-couchbase
-brew uninstall php56-event
-brew uninstall php56-gearman
-brew uninstall php56-geoip
-brew uninstall php56-imagick
-brew uninstall php56-mcrypt
-brew uninstall php56-mongodb
-brew uninstall php56-pdo-pgsql
-brew uninstall php56-redis
-brew uninstall php56-solr
-brew uninstall php56-xdebug
 
 # Remove composer
 rm -f /usr/local/bin/composer
