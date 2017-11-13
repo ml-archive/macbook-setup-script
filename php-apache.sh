26e3b113404a344e0624dcd1f00c2ae1fd009e87
Kirill Fuchs <kfuchs@fuzzproductions.com>
2017-11-13 12:53:03 -0500

    modified where apache is being setup by brew

--- php-apache.sh
+++ php-apache.sh
@@ -21,17 +21,17 @@ brew install httpd24 --with-privileged-ports --with-http2
 sudo brew services start homebrew/apache/httpd24
 
 # Modify Apache Configs
-sudo sed -i "" "s~Listen 8080~Listen 80~g" /usr/local/etc/apache2/2.4/httpd.conf # Ensure apache is set to port 80
-sudo sed -i "" "s~#LoadModule vhost_alias_module~LoadModule vhost_alias_module~g" /usr/local/etc/apache2/2.4/httpd.conf # Enable vhost module
-sudo sed -i "" "s~#LoadModule rewrite_module~LoadModule rewrite_module~g" /usr/local/etc/apache2/2.4/httpd.conf # Enable rewrite module
+sudo sed -i "" "s~Listen 8080~Listen 80~g" /usr/local/opt/apache2/httpd.conf # Ensure apache is set to port 80
+sudo sed -i "" "s~#LoadModule vhost_alias_module~LoadModule vhost_alias_module~g" /usr/local/opt/apache2/httpd.conf # Enable vhost module
+sudo sed -i "" "s~#LoadModule rewrite_module~LoadModule rewrite_module~g" /usr/local/opt/apache2/httpd.conf # Enable rewrite module
 
 # Setup apache to use proper Users and Group.
-sudo sed -i "" "s~User _www~User $USER~g" /usr/local/etc/apache2/2.4/httpd.conf
-sudo sed -i "" "s~Group _www~Group staff~g" /usr/local/etc/apache2/2.4/httpd.conf
-sudo sed -i "" "s~User daemon~User $USER~g" /usr/local/etc/apache2/2.4/httpd.conf
-sudo sed -i "" "s~Group daemon~Group staff~g" /usr/local/etc/apache2/2.4/httpd.conf
-sudo sed -i "" "s~/Library/WebServer/Documents~/Users/$USER/Sites~g" /usr/local/etc/apache2/2.4/httpd.conf
-sudo sed -i "" "s~/usr/local/var/www/htdocs~/Users/$USER/Sites~g" /usr/local/etc/apache2/2.4/httpd.conf
+sudo sed -i "" "s~User _www~User $USER~g" /usr/local/opt/apache2/httpd.conf
+sudo sed -i "" "s~Group _www~Group staff~g" /usr/local/opt/apache2/httpd.conf
+sudo sed -i "" "s~User daemon~User $USER~g" /usr/local/opt/apache2/httpd.conf
+sudo sed -i "" "s~Group daemon~Group staff~g" /usr/local/opt/apache2/httpd.conf
+sudo sed -i "" "s~/Library/WebServer/Documents~/Users/$USER/Sites~g" /usr/local/opt/apache2/httpd.conf
+sudo sed -i "" "s~/usr/local/var/www/htdocs~/Users/$USER/Sites~g" /usr/local/opt/apache2/httpd.conf
 
 # Set Apache to process php files.
 perl -i -0pe 's~<IfModule dir_module>\
@@ -43,21 +43,21 @@ perl -i -0pe 's~<IfModule dir_module>\
 \
 <FilesMatch \\.php\$>\
     SetHandler application/x-httpd-php\
-</FilesMatch>~' /usr/local/etc/apache2/2.4/httpd.conf
+</FilesMatch>~' /usr/local/opt/apache2/httpd.conf
 
 # Enable .htaccess files
-sudo sed -i "" "s~AllowOverride None~AllowOverride all~g" /usr/local/etc/apache2/2.4/httpd.conf
+sudo sed -i "" "s~AllowOverride None~AllowOverride all~g" /usr/local/opt/apache2/httpd.conf
 
 # Normalize logging to use /var/log/apache2
-sudo sed -i "" 's~ErrorLog "/usr/local/var/log/apache2/error_log"~ErrorLog "/var/log/apache2/error_log"~g' /usr/local/etc/apache2/2.4/httpd.conf
-sudo sed -i "" 's~CustomLog "/usr/local/var/log/apache2/access_log" common~CustomLog "/var/log/apache2/access_log" common~g' /usr/local/etc/apache2/2.4/httpd.conf
+sudo sed -i "" 's~ErrorLog "/usr/local/var/log/apache2/error_log"~ErrorLog "/var/log/apache2/error_log"~g' /usr/local/opt/apache2/httpd.conf
+sudo sed -i "" 's~CustomLog "/usr/local/var/log/apache2/access_log" common~CustomLog "/var/log/apache2/access_log" common~g' /usr/local/opt/apache2/httpd.conf
 
 # Add extra sites config 
-echo "Include /usr/local/etc/apache2/2.4/sites.conf" | sudo tee -a /usr/local/etc/apache2/2.4/httpd.conf
+echo "Include /usr/local/opt/apache2/sites.conf" | sudo tee -a /usr/local/opt/apache2/httpd.conf
 
 # Setup basic sites.conf vhosts.
-sudo touch /usr/local/etc/apache2/2.4/sites.conf
-sudo tee /usr/local/etc/apache2/2.4/sites.conf >/dev/null <<EOF
+sudo touch /usr/local/opt/apache2/sites.conf
+sudo tee /usr/local/opt/apache2/sites.conf >/dev/null <<EOF
 # Workaround for missing Authorization header under CGI/FastCGI Apache:
 <IfModule setenvif_module>
   SetEnvIf Authorization .+ HTTP_AUTHORIZATION=$0
@@ -103,13 +103,13 @@ brew install php70-redis
 brew install php70-xdebug
 
 # Rename to global brew location and Disable all php load modules.
-perl -i -0pe 's~LoadModule php5_module .*php55.*libphp5.so~#LoadModule php5_module    /usr/local/opt/php55/libexec/apache2/libphp5.so~' /usr/local/etc/apache2/2.4/httpd.conf
-perl -i -0pe 's~LoadModule php5_module .*php56.*libphp5.so~#LoadModule php5_module    /usr/local/opt/php56/libexec/apache2/libphp5.so~' /usr/local/etc/apache2/2.4/httpd.conf
-perl -i -0pe 's~LoadModule php7_module .*php70.*libphp7.so~#LoadModule php7_module    /usr/local/opt/php70/libexec/apache2/libphp7.so~' /usr/local/etc/apache2/2.4/httpd.conf
-perl -i -0pe 's~LoadModule php7_module .*php71.*libphp7.so~#LoadModule php7_module    /usr/local/opt/php71/libexec/apache2/libphp7.so~' /usr/local/etc/apache2/2.4/httpd.conf
+perl -i -0pe 's~LoadModule php5_module .*php55.*libphp5.so~#LoadModule php5_module    /usr/local/opt/php55/libexec/apache2/libphp5.so~' /usr/local/opt/apache2/httpd.conf
+perl -i -0pe 's~LoadModule php5_module .*php56.*libphp5.so~#LoadModule php5_module    /usr/local/opt/php56/libexec/apache2/libphp5.so~' /usr/local/opt/apache2/httpd.conf
+perl -i -0pe 's~LoadModule php7_module .*php70.*libphp7.so~#LoadModule php7_module    /usr/local/opt/php70/libexec/apache2/libphp7.so~' /usr/local/opt/apache2/httpd.conf
+perl -i -0pe 's~LoadModule php7_module .*php71.*libphp7.so~#LoadModule php7_module    /usr/local/opt/php71/libexec/apache2/libphp7.so~' /usr/local/opt/apache2/httpd.conf
 
 # Enable php70
-perl -i -0pe 's~#LoadModule php7_module    /usr/local/opt/php70/libexec/apache2/libphp7.so~LoadModule php7_module    /usr/local/opt/php70/libexec/apache2/libphp7.so~' /usr/local/etc/apache2/2.4/httpd.conf
+perl -i -0pe 's~#LoadModule php7_module    /usr/local/opt/php70/libexec/apache2/libphp7.so~LoadModule php7_module    /usr/local/opt/php70/libexec/apache2/libphp7.so~' /usr/local/opt/apache2/httpd.conf
 
 # restart apache and open phpinfo.
 sudo apachectl -k restart && open http://localhost/phpinfo.php
