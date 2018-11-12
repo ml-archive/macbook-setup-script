3956b87d256a25661435e0e5a3176cc19f68aa11
Kirill Fuchs <kfuchs@fuzzproductions.com>
2018-11-12 10:03:59 -0500

    removed copy pasted stuff

--- ruby/uninstall/run.sh
+++ ruby/uninstall/run.sh
@@ -7,8 +7,4 @@ if [ ! ${IS_COLOR_SOURCED} ]; then
     source ${DIR}/../../lib/colors.sh
 fi
 
-/usr/bin/yes | pip3 uninstall awscli
-/usr/bin/yes | pip3 uninstall awsebcli
-/usr/bin/yes | pip3 uninstall virtualenv
-brew uninstall python
-brew uninstall python@2
+# @TODO make uninstall
