3b5b0d6dc1aaf46a366df5afee1bb774f3d2a51c
Kirill Fuchs <kfuchs87@gmail.com>
2019-09-29 16:07:25 -0400

    Update run.sh

--- ruby/install/run.sh
+++ ruby/install/run.sh
@@ -8,13 +8,13 @@ fi
 
 echo "${GREEN}Installing... ${BROWN}ruby...${NC}"
 
-echo "${RED}Which ruby version manager would you like to use? (See readme for details) ${GREEN}[rbenv,rvm] default: rbenv${NC}"
+echo "${RED}Which ruby version manager would you like to use? (See readme for details) ${GREEN}[rbenv,rvm] default: rvm${NC}"
 read choice
 
 case $choice in
     rvm) source ${DIR}/install_rvm.sh;;
     rbenv) source ${DIR}/install_rbenv.sh ;;
-    *) source ${DIR}/install_rbenv.sh ;;
+    *) source ${DIR}/install_rvm.sh ;;
 esac
 
 gem install bundler
